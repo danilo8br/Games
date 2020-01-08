9febec01b4efa27e859cfc323252699e5b7a446f
danilo8br <51414398+danilo8br@users.noreply.github.com>
2020-01-08 20:42:29 -0300

    Update Jogo da adivinhação.py

--- Jogo da adivinhação.py
+++ Jogo da adivinhação.py
@@ -1,7 +1,7 @@
 from random import randint
 from time import sleep
 jogador = int(input("Em que numero eu pensei: ")) # Jogador tena adivinhar o número que o computador "pensou"
-computador = randint(0, 3)# Computador "pensa" alenatoriamente de 1 a 3
+computador = randint(0, 3)# Computador "pensa" aleatoriamente de 1 a 3
 print("Processando...")
 sleep(0.5)
 if jogador == computador:
